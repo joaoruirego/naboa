80709cd2565f2fd6cf6e72c247320adf071edbd1
joaoruirego <joaoruirego2@gmail.com>
2023-12-30 17:29:29 -0100

    desktop falta por o form

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,251 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import React, { useState, useEffect, useRef } from "react";
+
+const images = [
+  "/a.jpg",
+  "/b.jpg",
+  "/c.jpg",
+  "/d.jpg",
+  "/e.jpg",
+  "/f.jpg",
+  "/g.jpg",
+];
+const bgImages = [
+  "/bgNaboa1.jpg",
+  "/bgNaboa2.jpg",
+  "/bgNaboa3.jpg",
+  "/bgNaboa4.jpg",
+];
+const intervalTime = 2000;
 
 export default function Home() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [currentBgImage, setCurrentBgImage] = useState(0);
+  const carouselRef = useRef(null);
+
+  const nextImage = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  const nextBgImage = () => {
+    setCurrentBgImage((prev) => (prev + 1) % bgImages.length);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(nextImage, intervalTime);
+    const bgIntervalId = setInterval(nextBgImage, intervalTime);
+
+    return () => {
+      clearInterval(intervalId);
+      clearInterval(bgIntervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (carouselRef.current) {
+      carouselRef.current.style.transform = `translateX(${
+        -currentImage * 33.33
+      }%)`;
+    }
+  }, [currentImage]);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+      <div className={styles.nav}>
+        <img className={styles.logo} src="/logo.png" />
+        <ul className={styles.navUl}>
+          <li className={styles.navLi}>Home</li>
+          <li className={styles.navLi}>Localização</li>
+          <li className={styles.navLiE}>Encomendas</li>
+        </ul>
+      </div>
+      <div className={styles.titleZone}>
+        <h3 className={styles.sobreTitleTitle}>A Mercearia e Garrafeira</h3>
+        <h1 className={styles.title}>NABOA</h1>
+      </div>
+
+      <div className={styles.bgc}>
+        {bgImages.map((bgImage, index) => (
+          <img
+            key={index}
+            className={styles.bg}
+            src={bgImage}
+            alt={`Background Image ${index + 1}`}
+          />
+        ))}
+      </div>
+      <br></br>
+      <div className={styles.sobreContainer}>
+        <br></br>
+
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+          <h3 className={styles.sobreTitle}>A Mercearia e Garrafeira</h3>
+          <h2 className={styles.segTitle}>NABOA</h2>
+        </div>
+        <br></br>
+        <div>
+          <p className={styles.desc}>
+            Aqui vai ficar o texto número 1 sobre o Naboa Texto sobre o conceito
+            da mercearia Lorem ipsum dolor sit amet,<br></br> consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.<br></br> Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
+        <br></br>
+        <br></br>
+      </div>
+
+      <div className={styles.carouselContainer}>
+        <div ref={carouselRef} className={styles.carousel}>
+          {images.map((imageUrl, index) => (
+            <img
+              key={index}
+              src={imageUrl}
+              alt={`Image ${index + 1}`}
+              className={styles.carouselImage}
             />
-          </a>
+          ))}
+          {images.map((imageUrl, index) => (
+            <img
+              key={index + images.length}
+              src={imageUrl}
+              alt={`Image ${index + 1}`}
+              className={styles.carouselImage}
+            />
+          ))}
+          {images.map((imageUrl, index) => (
+            <img
+              key={index + 2 * images.length}
+              src={imageUrl}
+              alt={`Image ${index + 1}`}
+              className={styles.carouselImage}
+            />
+          ))}
         </div>
       </div>
+      <br></br>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.sobreContainer}>
+        <br></br>
+
+        <h2 style={{ fontSize: 75 }} className={styles.segTitle}>
+          A Carta
+        </h2>
+        <br></br>
+        <div>
+          <p className={styles.desc}>
+            Pequeno texto sobre a carta Lorem ipsum dolor sit amet,<br></br>{" "}
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.buttonMenu}>Menu</button>
+          <button className={styles.buttonVinhos}>Vinhos</button>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <div className={styles.carouselContainer}>
+        <div ref={carouselRef} className={styles.carousel}>
+          {images.map((imageUrl, index) => (
+            <img
+              key={index}
+              src={imageUrl}
+              alt={`Image ${index + 1}`}
+              className={styles.carouselImage}
+            />
+          ))}
+          {images.map((imageUrl, index) => (
+            <img
+              key={index + images.length}
+              src={imageUrl}
+              alt={`Image ${index + 1}`}
+              className={styles.carouselImage}
+            />
+          ))}
+          {images.map((imageUrl, index) => (
+            <img
+              key={index + 2 * images.length}
+              src={imageUrl}
+              alt={`Image ${index + 1}`}
+              className={styles.carouselImage}
+            />
+          ))}
+        </div>
+      </div>
+      <br></br>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      <div className={styles.sobreContainer}>
+        <br></br>
+        <h2 style={{ fontSize: 75 }} className={styles.segTitle}>
+          Encomendas
+        </h2>
+        <br></br>
+        <div>
+          <br></br>
+          <p className={styles.desc}>
+            Pequeno texto sobre as encomendas Lorem ipsum dolor sit amet,
+            <br></br> consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
           </p>
-        </a>
+        </div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className={styles.formStruct}>
+          <div className={styles.formStructDiv}>
+            <div className={styles.inputs}>
+              <input placeholder="Nome"></input>
+              <br></br>
+              <input placeholder="Email"></input>
+              <br></br>
+              <input placeholder="Telemóvel"></input>
+            </div>
+            <textarea
+              placeholder="Mensagem"
+              className={styles.textArea}
+            ></textarea>
+          </div>
+        </div>
+
+        <div className={styles.footer}>
+          <div className={styles.zonasFooter}>
+            <div style={{ width: "33.3%" }}>
+              <h3 className={styles.titleForm}>Onde estamos</h3>
+              <p className={styles.descForm}>
+                Rua Professor Fernando da Fonseca 25A <br></br>1600-617 Lisboa
+              </p>
+            </div>
+            <div style={{ width: "33.3%" }}>
+              <h3 className={styles.titleForm}>Contactos</h3>
+              <p className={styles.descForm}>
+                +351 964 217 459
+                <br></br>info@mercadonaboa.pt
+              </p>
+            </div>
+            <div style={{ width: "33.3%" }}>
+              <h3 className={styles.titleForm}>Redes Sociais</h3>
+              <p className={styles.descForm}>
+                Instagram
+                <br></br>Facebook
+              </p>
+            </div>
+          </div>
+          <br></br>
+          <div className={styles.logoFooterImg}>
+            <img className={styles.logoFooter} src="/logo.png" />
+          </div>
+        </div>
+        <br></br>
       </div>
     </main>
-  )
+  );
 }
