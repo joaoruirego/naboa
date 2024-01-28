3c920974bcee67ef485eeae1d47633356f67c2db
joaoruirego <joaoruirego2@gmail.com>
2024-01-28 18:55:05 +0000

    vinhos atualizados

--- pages/vinhos.js
+++ pages/vinhos.js
@@ -4,18 +4,18 @@ import styles from "../src/app/vinhos.module.css"; // Certifique-se de que este 
 
 const MenuPage = () => {
   const images = [
-    "/e.jpg",
-    "/f.jpg",
-    "/g.jpg",
-    "/e.jpg",
-    "/f.jpg",
-    "/g.jpg",
-    "/e.jpg",
-    "/f.jpg",
-    "/g.jpg",
-    "/f.jpg",
-    "/g.jpg",
-    "/e.jpg",
+    "/1.jpg",
+    "/2.jpg",
+    "/3.jpg",
+    "/4.jpg",
+    "/5.jpg",
+    "/6.jpg",
+    "/7.jpg",
+    "/8.jpg",
+    "/9.jpg",
+    "/10.jpg",
+    "/11.jpg",
+    "/12.jpg",
   ];
 
   return (
